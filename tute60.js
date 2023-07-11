71718adae3f354fa5db76c91634345f952e69536
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-07-11 21:07:40 +0600

    adding fles

---
+++ tute60.js
@@ -0,0 +1,34 @@
+    // Arrow function
+    // let greet =  ()=> {
+    //     console.log('Good morning');
+    // }
+
+    let greet =  () => console.log('Good morning');
+    
+
+    // let sum2 = (a, b)=>{
+    //     return a+b;
+    // };
+
+    let sum2 = (a, b) => a+b; 
+    let half = a => a/2;  
+
+    // function greet() {
+    //     console.log('Good morning');
+    // }
+
+    greet();
+    setTimeout(() => {
+        console.log("We are inside settimeout");
+    }, 3000);
+
+    let obj1={
+        greeting: "Good Morning",
+        names: ["Harry", "Rohan", "SkillF", "DjKhiladi"],
+        speak(){
+            this.names.forEach((student)=>{
+                console.log(this.greeting + " Kukdoo Koo " + student);
+            });
+        }
+    }
+    obj1.speak();
