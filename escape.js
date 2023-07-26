2927d7c0ead3ea0a4c9719533a4bea69efaf0b2e
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-07-26 21:12:35 +0600

    added files

---
+++ escape.js
@@ -0,0 +1,28 @@
+// // In JavaScript, an escape character is a special character that is used to represent characters that would otherwise be difficult or impossible to include directly in a string.Escape characters are preceded by a backslash(`\`) to give them a special meaning.
+
+// // The most common use of escape characters is to represent characters with special meanings in strings, such as quotes or newlines, or to represent characters that are not directly typable, such as certain control characters.
+
+// // Here are some common escape characters in JavaScript:
+
+// // 1. `\'`: Single quote (apostrophe)
+// // 2. `\"`: Double quote
+// // 3. `\\`: Backslash
+// // 4. `\n`: Newline
+// // 5. `\r`: Carriage return
+// // 6. `\t`: Tab
+// // 7. `\b`: Backspace
+// // 8. `\f`: Form feed(used mostly in older systems)
+// // 9. `\uXXXX`: Unicode escape, where XXXX represents the Unicode code point in hexadecimal
+
+// // Here are some examples of using escape characters in JavaScript strings:
+
+// // ```javascript
+// let singleQuoteString = 'This is a single quote: \'escaped\'';
+// let doubleQuoteString = "This is a double quote: \"escaped\"";
+// let backslashString = "This contains a backslash: \\";
+// let newlineString = "This has\na newline";
+// let unicodeString = "This is a snowman: \u2603";
+// ```
+
+// Using escape characters allows you to include special characters or control 
+//  characters within a string without causing syntax errors or unwanted behavior in your code.
---
+++ events.js
@@ -0,0 +1,10 @@
+// EVENTS NAME IS JS AND WHAT THEY DO
+
+
+// Event --	Description
+// onchange =	An HTML element has been changed
+// onclick	= The user clicks an HTML element
+// onmouseover =	The user moves the mouse over an HTML element
+// onmouseout =	The user moves the mouse away from an HTML element
+// onkeydown =	The user pushes a keyboard key
+// onload	 The browser has finished loading the page
