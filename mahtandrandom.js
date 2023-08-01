4594b816caba16f61a7a0aaa8e12167c99c8efec
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-08-01 19:55:10 +0600

    adding files

---
+++ mahtandrandom.js
@@ -0,0 +1,31 @@
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+
+// Returns a random integer from 0 to 9:
+Math.floor(Math.random() * 10);
+
+// Returns a random integer from 0 to 10:
+Math.floor(Math.random() * 11);
+
+// Returns a random integer from 0 to 100:
+Math.floor(Math.random() * 101);
+
+// Returns a random integer from 1 to 10:
+Math.floor(Math.random() * 10) + 1;
+
+// Returns a random integer from 1 to 100:
+Math.floor(Math.random() * 100) + 1;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }  
