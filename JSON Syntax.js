14aefa49940238d84324ae6e1cb373681ab4d691
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-08-29 20:00:45 +0600

    adding files

---
+++ JSON Syntax.js
@@ -0,0 +1,39 @@
+// JSON Syntax Rules
+// JSON syntax is derived from JavaScript object notation syntax:
+
+// Data is in name/value pairs
+// Data is separated by commas
+// Curly braces hold objects
+// Square brackets hold arrays
+// JSON Data - A Name and a Value
+// JSON data is written as name/value pairs (aka key/value pairs).
+
+// A name/value pair consists of a field name (in double quotes), followed by a colon, followed by a value:
+
+// Example
+"name":"John"
+// JSON Values
+// In JSON, values must be one of the following data types:
+
+// a string
+// a number
+// an object
+// an array
+// a boolean
+// null
+// In JavaScript values can be all of the above, plus any other valid JavaScript expression, including:
+
+// a function
+// a date
+// undefined
+// In JSON, string values must be written with double quotes:
+
+// JSON
+{"name":"John"}
+
+// Because JSON syntax is derived from JavaScript object notation, very little extra software is needed to work with JSON within JavaScript.
+
+// With JavaScript you can create an object and assign data to it, like this:
+
+// Example
+person = {name:"John", age:31, city:"New York"};
