b202e381c019e3a43f7d65709b46a4f4be538f1a
nh246 <nazmul210130@diit.edu.bd>
2023-11-21 21:35:36 +0600

    adding code

--- forloop.js
+++ forloop.js
@@ -14,3 +14,28 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // <ul><li>Banana</li><li>Orange</li><li>Apple</li>
 // <ul><li>Banana</li><li>Orange</li><li>Apple</li><li>Mango</li>
 // <ul><li>Banana</li><li>Orange</li><li>Apple</li><li>Mango</li></ul>
+
+// ========== for of loop =========== 
+
+const arr =[1,2,3,4,5,]
+
+for (const num of arr) {
+  console.log(num);
+}
+
+
+// ======= for in loop ======== 
+
+
+
+
+// ======= foreach loop ========
+const coding =["jk","ruby","cpp","run"]
+
+// coding.forEach( function (item){
+// console.log(item);
+// } )
+
+coding.forEach((item)=>{
+console.log(item);
+})
